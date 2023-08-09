faf3c7412072f880aba7b6f91bf14bf831e2a0bf
Shahab Mohiti <s.mohiti@asax.ir>
2023-08-09 10:07:22 +0330

    login and cart list

---
+++ src/app/admin/cart/page.tsx
@@ -0,0 +1,179 @@
+export default function Cart() {
+    return (
+        <div className="py-10 bg-gray-100">
+            <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+                <table className="min-w-full border-collapse block md:table">
+                    <thead className="block md:table-header-group">
+                        <tr className="border border-grey-500 md:border-none block md:table-row absolute -top-full md:top-auto -left-full md:left-auto  md:relative ">
+                            <th className="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">
+                                Product Name
+                            </th>
+                            <th className="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">
+                                User Name
+                            </th>
+                            <th className="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">
+                                Email Address
+                            </th>
+                            <th className="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">
+                                Mobile
+                            </th>
+                            <th className="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">
+                                Actions
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody className="block md:table-row-group">
+                        <tr className="bg-gray-300 border border-grey-500 md:border-none block md:table-row">
+                            <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell">
+                                <span className="inline-block w-1/3 md:hidden font-bold">
+                                    Name
+                                </span>
+                                Jamal Rios
+                            </td>
+                            <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell">
+                                <span className="inline-block w-1/3 md:hidden font-bold">
+                                    User Name
+                                </span>
+                                jrios1
+                            </td>
+                            <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell">
+                                <span className="inline-block w-1/3 md:hidden font-bold">
+                                    Email Address
+                                </span>
+                                jrios@icloud.com
+                            </td>
+                            <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell">
+                                <span className="inline-block w-1/3 md:hidden font-bold">
+                                    Mobile
+                                </span>
+                                582-3X2-6233
+                            </td>
+                            <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell">
+                                <span className="inline-block w-1/3 md:hidden font-bold">
+                                    Actions
+                                </span>
+                                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 border border-blue-500 rounded">
+                                    Approve
+                                </button>
+                                <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 border border-red-500 rounded">
+                                    Reject
+                                </button>
+                            </td>
+                        </tr>
+                        <tr className="bg-white border border-grey-500 md:border-none block md:table-row">
+                            <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell">
+                                <span className="inline-block w-1/3 md:hidden font-bold">
+                                    Name
+                                </span>
+                                Erwin Campbell
+                            </td>
+                            <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell">
+                                <span className="inline-block w-1/3 md:hidden font-bold">
+                                    User Name
+                                </span>
+                                ecampbell088
+                            </td>
+                            <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell">
+                                <span className="inline-block w-1/3 md:hidden font-bold">
+                                    Email Address
+                                </span>
+                                ecampbell088@hotmail.com
+                            </td>
+                            <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell">
+                                <span className="inline-block w-1/3 md:hidden font-bold">
+                                    Mobile
+                                </span>
+                                318-685-X414
+                            </td>
+                            <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell">
+                                <span className="inline-block w-1/3 md:hidden font-bold">
+                                    Actions
+                                </span>
+                                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 border border-blue-500 rounded">
+                                    Approve
+                                </button>
+                                <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 border border-red-500 rounded">
+                                    Reject
+                                </button>
+                            </td>
+                        </tr>
+                        <tr className="bg-gray-300 border border-grey-500 md:border-none block md:table-row">
+                            <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell">
+                                <span className="inline-block w-1/3 md:hidden font-bold">
+                                    Name
+                                </span>
+                                Lillie Clark
+                            </td>
+                            <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell">
+                                <span className="inline-block w-1/3 md:hidden font-bold">
+                                    User Name
+                                </span>
+                                lillie
+                            </td>
+                            <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell">
+                                <span className="inline-block w-1/3 md:hidden font-bold">
+                                    Email Address
+                                </span>
+                                lillie.clark@gmail.com
+                            </td>
+                            <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell">
+                                <span className="inline-block w-1/3 md:hidden font-bold">
+                                    Mobile
+                                </span>
+                                505-644-84X4
+                            </td>
+                            <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell">
+                                <span className="inline-block w-1/3 md:hidden font-bold">
+                                    Actions
+                                </span>
+                                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 border border-blue-500 rounded">
+                                    Approve
+                                </button>
+                                <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 border border-red-500 rounded">
+                                    Reject
+                                </button>
+                            </td>
+                        </tr>
+                        <tr className="bg-white border border-grey-500 md:border-none block md:table-row">
+                            <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell">
+                                <span className="inline-block w-1/3 md:hidden font-bold">
+                                    Name
+                                </span>
+                                Maribel Koch
+                            </td>
+                            <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell">
+                                <span className="inline-block w-1/3 md:hidden font-bold">
+                                    User Name
+                                </span>
+                                maribelkoch
+                            </td>
+                            <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell">
+                                <span className="inline-block w-1/3 md:hidden font-bold">
+                                    Email Address
+                                </span>
+                                mkoch@yahoo.com
+                            </td>
+                            <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell">
+                                <span className="inline-block w-1/3 md:hidden font-bold">
+                                    Mobile
+                                </span>
+                                582-400-3X36
+                            </td>
+                            <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell">
+                                <span className="inline-block w-1/3 md:hidden font-bold">
+                                    Actions
+                                </span>
+                                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 border border-blue-500 rounded">
+                                    Approve
+                                </button>
+                                <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 border border-red-500 rounded">
+                                    Reject
+                                </button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+}
